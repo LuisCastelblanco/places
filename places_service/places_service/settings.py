7f8e46727b0b908b0736cc6efb3c5832ccb92094
LuisCastelblanco <la.castelblanco@uniandes.edu.co>
2024-05-19 14:09:29 -0500

    chore: Add rest_framework and places app to installed apps

--- places_service/places_service/settings.py
+++ places_service/places_service/settings.py
@@ -37,6 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'rest_framework',
+    'places', # Add the places app to the list of installed apps
 ]
 
 MIDDLEWARE = [

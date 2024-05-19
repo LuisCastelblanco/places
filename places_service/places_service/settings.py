1a301bf418ca9703ed15667099a090073d5c0706
LuisCastelblanco <la.castelblanco@uniandes.edu.co>
2024-05-19 14:19:15 -0500

    chore: Update database engine to use psycopg2

--- places_service/places_service/settings.py
+++ places_service/places_service/settings.py
@@ -78,7 +78,7 @@ WSGI_APPLICATION = 'places_service.wsgi.application'
 # places_service/settings.py
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
         'NAME': 'places_db',
         'USER': 'places_user',
         'PASSWORD': 'isis2503',

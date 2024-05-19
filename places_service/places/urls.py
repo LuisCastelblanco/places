e16ec6be3d7efae62f6045d2bbd24709bb3bc83a
LuisCastelblanco <la.castelblanco@uniandes.edu.co>
2024-05-19 14:02:39 -0500

    chore: Add initial Django files and configurations

---
+++ places_service/places/urls.py
@@ -0,0 +1,9 @@
+
+from django.urls import path, include
+from rest_framework.routers import DefaultRouter
+from .views import PlaceViewSet
+
+router = DefaultRouter()
+router.register(r'places', PlaceViewSet)
+
+urlpatterns = router.urls
